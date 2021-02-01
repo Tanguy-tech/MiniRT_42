9ad62ce79590fb0ba0cbb47ebfb24e8ce7ed107b
Tanguy BILLON <tanguy_billon@hotmail.fr>
2021-02-01 14:41:31 +0100

    parsing errors

--- srcs/miniRT.c
+++ srcs/miniRT.c
@@ -6,7 +6,7 @@
 /*   By: tbillon <tbillon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/01 12:53:23 by tbillon           #+#    #+#             */
-/*   Updated: 2021/02/01 14:25:33 by tbillon          ###   ########lyon.fr   */
+/*   Updated: 2021/02/01 14:40:13 by tbillon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		main(int ac, char **av)
 {
 	if (errors_check(ac, av) == 0) /* if no errors detected, then open and read file.rt */
 	{
-		/* Check if file.rt exist, if file exist then execute it, otherwise print ERROR.*/
+		/* Check if file.rt exist, if file exist then read and execute it, otherwise print ERROR.*/
 	}
 	return (0);
 }
