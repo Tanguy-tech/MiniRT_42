8ae611fe9ab6be29340ef61653c46c64aa1b7214
Tanguy BILLON <tanguy_billon@hotmail.fr>
2021-02-06 14:18:55 +0100

    change vector initialization from 0 to 0.0 (double)

--- srcs/vectors.c
+++ srcs/vectors.c
@@ -6,7 +6,7 @@
 /*   By: tbillon <tbillon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 09:05:36 by tbillon           #+#    #+#             */
-/*   Updated: 2021/02/06 10:41:52 by tbillon          ###   ########lyon.fr   */
+/*   Updated: 2021/02/06 14:18:01 by tbillon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,10 @@ t_vectors	*initialize_vector(void)
 
 	if (!(new_vector = ft_calloc(sizeof(t_vectors), 1)))
 		return (NULL);
-	new_vector->x = 0;
-	new_vector->y = 0;
-	new_vector->z = 0;
-	new_vector->sqrt_norme = 0;
-	new_vector->norme = 0;
+	new_vector->x = 0.0;
+	new_vector->y = 0.0;
+	new_vector->z = 0.0;
+	new_vector->sqrt_norme = 0.0;
+	new_vector->norme = 0.0;
 	return (new_vector);
 }
