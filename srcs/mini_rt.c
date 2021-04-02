0dd86fae8dd7aacdd430bfa820345b38f892b921
Tanguy BILLON <tanguy_billon@hotmail.fr>
2021-04-02 11:06:01 +0200

    window close click on red cross

--- srcs/mini_rt.c
+++ srcs/mini_rt.c
@@ -6,7 +6,7 @@
 /*   By: tbillon <tbillon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/01 12:53:23 by tbillon           #+#    #+#             */
-/*   Updated: 2021/04/02 09:46:14 by tbillon          ###   ########lyon.fr   */
+/*   Updated: 2021/04/02 11:05:23 by tbillon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@ int	close_win(int keycode, t_scene *rt)
 		exit(0);
 	}
 	return (0);
+}
+
+int	close_cross(t_scene *rt)
+{
+	mlx_destroy_window(rt->mlx_ptr, rt->win_ptr);
+	exit(0);
 }
 
 void	create_window(t_scene *rt)
@@ -36,6 +42,7 @@ void	create_window(t_scene *rt)
 		rt->img->ptr,
 		0, 0);
 	mlx_hook(rt->win_ptr, 2, 1L << 0, close_win, rt);
+	mlx_hook(rt->win_ptr, 17, 1L << 17, close_cross, rt);
 	mlx_loop(rt->mlx_ptr);
 }
 
